39abbcae978cb7c57635f09ddfa2da6c29f4b9c8
Tassu <git@tassu.me>
2018-05-31 16:38:57 +0300

    reformat (again) using XOJS
    Signed-off-by: Tassu <git@tassu.me>

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
 const express = require('express');
+
 const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
 
-let foodList = {
-  "meatfood": [],
-  "vegefood": []
-}
+const foodList = {
+	meatfood: [],
+	vegefood: []
+};
 
 function updateFoodlist() {
-  let url = 'https://www.mayk.fi/tietoa-meista/ruokailu/';
+	const url = 'https://www.mayk.fi/tietoa-meista/ruokailu/';
 
-  request(url, function (error, response, html) {
-    if (!error) {
-      var $ = cheerio.load(html);
+	request(url, (error, response, html) => {
+		if (!error) {
+			const $ = cheerio.load(html);
 
-      var vegedays = $('.ruoka-header-kasvisruoka');
-      vegedays.each(function (i, elem) {
-        foodList
-          .vegefood
-          .push({
-            "day": elem
-              .prev
-              .prev
-              .prev
-              .prev
-              .children[0]
-              .data
-              .slice(1, -1),
-            "food": elem.children[0].data
-          })
-      })
+			const vegedays = $('.ruoka-header-kasvisruoka');
+			vegedays.each((i, elem) => {
+				foodList
+					.vegefood
+					.push({
+						day: elem
+							.prev
+							.prev
+							.prev
+							.prev
+							.children[0]
+							.data
+							.slice(1, -1),
+						food: elem.children[0].data
+					});
+			});
 
-      var meatdays = $('.ruoka-header-ruoka');
-      meatdays.each(function (i, elem) {
-        foodList
-          .meatfood
-          .push({
-            "day": elem
-              .prev
-              .prev
-              .children[0]
-              .data
-              .slice(1, -1),
-            "food": elem.children[0].data
-          })
-      })
-      console.log("List updated")
-    }
-  })
+			const meatdays = $('.ruoka-header-ruoka');
+			meatdays.each((i, elem) => {
+				foodList
+					.meatfood
+					.push({
+						day: elem
+							.prev
+							.prev
+							.children[0]
+							.data
+							.slice(1, -1),
+						food: elem.children[0].data
+					});
+			});
+			console.log('List updated');
+		}
+	});
 }
 
 updateFoodlist();
 
-setInterval(function () {
-  updateFoodlist();
+setInterval(() => {
+	updateFoodlist();
 }, 1 * 60 * 60 * 1000);
 
-app.get('/api/food/', (req, res) => res.send(foodList))
-app.get('/api/food/today', (req, res) => res.send({meatfood: foodList.meatfood[foodList.meatfood.length - 1], vegefood: foodList.vegefood[foodList.vegefood.length - 1]}))
+app.get('/api/food/', (req, res) => res.send(foodList));
+app.get('/api/food/today', (req, res) => res.send({meatfood: foodList.meatfood[foodList.meatfood.length - 1], vegefood: foodList.vegefood[foodList.vegefood.length - 1]}));
 
-app.listen(8585, () => console.log('SHIT GOING ON ON 8585 OH SHIT'))
+app.listen(8585, () => console.log('SHIT GOING ON ON 8585 OH SHIT'));
