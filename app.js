eba9f8271ea9c07b96eb6547be9f63265402d017
Tassu <git@tassu.me>
2018-05-31 16:28:15 +0300

    fixed formatting & stuff
    Signed-off-by: Tassu <git@tassu.me>

--- app.js
+++ app.js
@@ -3,41 +3,58 @@ const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
 
-foodList = {
+let foodList = {
   "meatfood": [],
   "vegefood": []
 }
 
 function updateFoodlist() {
-  url = 'https://www.mayk.fi/tietoa-meista/ruokailu/';
+  let url = 'https://www.mayk.fi/tietoa-meista/ruokailu/';
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
 
-            var vegedays = $('.ruoka-header-kasvisruoka');
-            vegedays.each(
-              function(i, elem) {
-                foodList.vegefood.push({"day": elem.prev.prev.prev.prev.children[0].data.slice(1, -1), "food": elem.children[0].data})
-              }
-            )
+      var vegedays = $('.ruoka-header-kasvisruoka');
+      vegedays.each(function (i, elem) {
+        foodList
+          .vegefood
+          .push({
+            "day": elem
+              .prev
+              .prev
+              .prev
+              .prev
+              .children[0]
+              .data
+              .slice(1, -1),
+            "food": elem.children[0].data
+          })
+      })
 
-
-            var meatdays = $('.ruoka-header-ruoka');
-            meatdays.each(
-              function(i, elem) {
-                foodList.meatfood.push({"day": elem.prev.prev.children[0].data.slice(1, -1), "food": elem.children[0].data})
-              }
-            )
-            console.log("List updated")
-        }
-    })
+      var meatdays = $('.ruoka-header-ruoka');
+      meatdays.each(function (i, elem) {
+        foodList
+          .meatfood
+          .push({
+            "day": elem
+              .prev
+              .prev
+              .children[0]
+              .data
+              .slice(1, -1),
+            "food": elem.children[0].data
+          })
+      })
+      console.log("List updated")
+    }
+  })
 }
 
 updateFoodlist();
 
 setInterval(function () {
-   updateFoodlist();
+  updateFoodlist();
 }, 1 * 60 * 60 * 1000);
 
 app.get('/api/food/', (req, res) => res.send(foodList))
